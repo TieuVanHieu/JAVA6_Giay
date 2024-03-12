72f383ad3dba65bd748eed57d828749cb5977fce
TranPhuocToan <tranphuoctoan12122003@gmail.com>
2024-03-12 15:23:05 +0700

    .

--- src/main/java/com/example/demo/controller/user/Userhome.java
+++ src/main/java/com/example/demo/controller/user/Userhome.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class Userhome {
     
+    @RequestMapping("/index")
+    public String index(){
+        return "user/index";
+    }
 }
