0cddb3931cb4edcf4453bf71ea8c96bb23d42cca
TranPhuocToan <tranphuoctoan12122003@gmail.com>
2024-03-14 17:45:34 +0700

    active shipping

--- src/main/java/com/example/demo/model/ShippingInfoEntity.java
+++ src/main/java/com/example/demo/model/ShippingInfoEntity.java
@@ -46,6 +46,8 @@ public class ShippingInfoEntity {
     @Pattern(regexp = "^(0[3|5|7|8|9])+([0-9]{8})", message = "Số điện thoại di động không hợp lệ!")
     String shippingSdt;
 
+    Boolean active = false;
+
     @JsonIgnore
     @OneToMany(mappedBy = "shippingInfo")
     List<OrderEntity> order;
