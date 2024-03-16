a4e6c0e6b77487d042a838d46f53f0045f454f79
tieuvanhieu2003 <hieutvpc04646@fpt.edu.vn>
2024-03-16 22:28:55 +0700

    giao dien user

--- src/main/java/com/example/demo/controller/user/Userhome.java
+++ src/main/java/com/example/demo/controller/user/Userhome.java
@@ -5,9 +5,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Userhome {
-    
-    @RequestMapping("/index")
-    public String index(){
-        return "user/index";
-    }
+	@RequestMapping("/index")
+	public String index() {
+		return "user/index";
+	}
+
+	@RequestMapping("/login")
+	public String login() {
+		return "user/login";
+	}
+
+	@RequestMapping("/product")
+	public String product() {
+		return "user/product";
+	}
+
+	@RequestMapping("/cart")
+	public String cart() {
+		return "user/cart";
+	}
+
+	@RequestMapping("/checkout")
+	public String checkout() {
+		return "user/checkout";
+	}
+
+	@RequestMapping("/category")
+	public String category() {
+		return "user/category";
+	}
+
+	@RequestMapping("/dashboard")
+	public String dashboard() {
+		return "user/dashboard";
+	}
+	@RequestMapping("/wishlist")
+	public String wishlist() {
+		return "user/wishlist";
+	}
+	@RequestMapping("/category-boxed")
+	public String categoryBoxed() {
+		return "user/category-boxed";
+	}
+	
 }
