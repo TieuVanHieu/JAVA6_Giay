04cc214e0711eb7cf383ff7f4bf812b26313f6db
TranPhuocToan <tranphuoctoan12122003@gmail.com>
2024-03-21 13:08:42 +0700

    keo tu dev chinh giao dien

--- src/main/java/com/example/demo/controller/admin/CategoryController.java
+++ src/main/java/com/example/demo/controller/admin/CategoryController.java
@@ -1,4 +1,4 @@
-package com.example.demo.AdminController;
+package com.example.demo.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
