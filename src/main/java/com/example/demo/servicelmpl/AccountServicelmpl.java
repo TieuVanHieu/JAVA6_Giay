cbedfbf9e139894a586c1df5db39ebd4408de161
sasaki1999 <syole188@gmail.com>
2024-04-07 19:12:22 +0700

    admin

--- src/main/java/com/example/demo/servicelmpl/AccountServicelmpl.java
+++ src/main/java/com/example/demo/servicelmpl/AccountServicelmpl.java
@@ -1,15 +1,32 @@
 package com.example.demo.servicelmpl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.model.AccountEntity;
 import com.example.demo.repository.AccountEntityDAO;
 import com.example.demo.service.AccountService;
 
 
-
+@Service
 public class AccountServicelmpl implements AccountService {
+
+    @Autowired
+    AccountEntityDAO accountEntityDAO;
+
+    @Override
+    public List<AccountEntity> findAll() {
+        // TODO Auto-generated method stub
+        return accountEntityDAO.findAll();
+    }
+
+    @Override
+    public Optional<AccountEntity> findById(String userName) {
+        // TODO Auto-generated method stub
+       return accountEntityDAO.findById(userName);
+    }
     
 }
